137abae2bcab0afed0a10fa9f78e76fc1a3df5ee
shtksm <143459053+shtksm@users.noreply.github.com>
2024-12-15 11:34:44 +0900

    修正分

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,16 +1,12 @@
-let num = 15 ;
-if (num % 3 == 0 ){
-console.log("３の倍数です")
-}
-if(num % 5 == 0){
-    console.log("５の倍数です")
-}
+let num = 15
 
-if(num % 15 == 0){
-    console.log("３と５の倍数です")
-}
-
-else{
-    console.log(num)
-}
-
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+  } else if (num % 3 === 0) {
+    console.log('3の倍数です');
+  } else if (num % 5 === 0) {
+    console.log('5の倍数です');
+  } else {
+    console.log(num);
+  }
+  
