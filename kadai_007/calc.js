9bcb8c6f4bf8fcc0b633a34eb81a0feaa853d751
shtksm <143459053+shtksm@users.noreply.github.com>
2024-12-15 11:03:02 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+let num = 15 ;
+if (num % 3 == 0 ){
+console.log("３の倍数です")
+}
+if(num % 5 == 0){
+    console.log("５の倍数です")
+}
+
+if(num % 15 == 0){
+    console.log("３と５の倍数です")
+}
+
+else{
+    console.log(num)
+}
+
